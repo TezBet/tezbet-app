ee1e6fee14ecae705f1bd285aaa12175117accfc
Paralogoss <30406089+Paralogoss@users.noreply.github.com>
2021-12-12 19:20:40 +0100

    Format fix

--- src/components/Faucet.tsx
+++ src/components/Faucet.tsx
@@ -1,13 +1,13 @@
-import { useState, Fragment, useContext } from 'react';
-import { TezosToolkit } from '@taquito/taquito';
-import { InMemorySigner } from '@taquito/signer';
-import { Button, Modal } from 'react-bootstrap';
-import { ReactComponent as BankIcon } from 'bootstrap-icons/icons/bank.svg';
+import { useState, Fragment, useContext } from "react";
+import { TezosToolkit } from "@taquito/taquito";
+import { InMemorySigner } from "@taquito/signer";
+import { Button, Modal } from "react-bootstrap";
+import { ReactComponent as BankIcon } from "bootstrap-icons/icons/bank.svg";
 
-import { WalletContext } from '../utils/WalletContextProvider';
-import { getExplorerLink, shortenString } from '../utils/utils';
+import { WalletContext } from "../utils/WalletContextProvider";
+import { getExplorerLink, shortenString } from "../utils/utils";
 
-function Faucet(props:any) {
+function Faucet(props: any) {
     const [transactionHash, setTransactionHash] = useState("");
     const [error, setError] = useState("");
     const [step, setStep] = useState(0);
@@ -24,60 +24,93 @@ function Faucet(props:any) {
     const handleClose = () => {
         if (step === 3) setStep(0);
         setShow(false);
-    }
+    };
 
     const drip = () => {
         if (address == null) return;
         setShow(true);
         if (step !== 0) return;
         setStep(1);
-        
+
         InMemorySigner.fromSecretKey(process.env.REACT_APP_FAUCET_PRIVATE_KEY!)
-        .then((signer) => {
-            Tezos.setProvider({ signer: signer });
-        }).then(() => Tezos.contract.transfer({ to: address, amount: amount }))
-        .then((op) => {
-            setStep(2);
-            setTransactionHash(op.hash);
-            return op.confirmation(1);
-        }).then(() => {
-            setStep(3);
-            refreshBalance();
-        })
-        .catch((err) => setError(JSON.stringify(err, null, 2)));
-    }
+            .then((signer) => {
+                Tezos.setProvider({ signer: signer });
+            })
+            .then(() =>
+                Tezos.contract.transfer({ to: address, amount: amount })
+            )
+            .then((op) => {
+                setStep(2);
+                setTransactionHash(op.hash);
+                return op.confirmation(1);
+            })
+            .then(() => {
+                setStep(3);
+                refreshBalance();
+            })
+            .catch((err) => setError(JSON.stringify(err, null, 2)));
+    };
 
     return (
         <Fragment>
-            <Button onClick={() => setShow(true)} variant="dark" style={{ marginRight: "10px"}}>
-                <BankIcon width="18px" height="18px" style={{ top: "-3px", position: "relative"}} /> FAUCET
+            <Button
+                onClick={() => setShow(true)}
+                variant="dark"
+                style={{ marginRight: "10px" }}
+            >
+                <BankIcon
+                    width="18px"
+                    height="18px"
+                    style={{ top: "-3px", position: "relative" }}
+                />{" "}
+                FAUCET
             </Button>
 
-            <Modal contentClassName="faucet-modal" show={show} onHide={handleClose} onShow={drip} size="lg">
+            <Modal
+                contentClassName="faucet-modal"
+                show={show}
+                onHide={handleClose}
+                onShow={drip}
+                size="lg"
+            >
                 <Modal.Header closeButton>
                     <Modal.Title>Hangzhounet XTZ Faucet</Modal.Title>
                 </Modal.Header>
                 <Modal.Body id="faucet-modal-body">
-                    <p>Transfering {amount} ꜩ to {shortenString(address, 8)}.</p>
-                    { step >= 2 &&
+                    <p>
+                        Transfering {amount} ꜩ to {shortenString(address, 8)}.
+                    </p>
+                    {step >= 2 && (
                         <Fragment>
-                            <p>Waiting for 
-                                <a rel="noreferrer" target="_blank" href={tzlink}> {shortenString(transactionHash, 8)} </a> 
-                                to be confirmed.</p>
-                            <p>You can close this window. The transaction will be processed in the background.</p>
+                            <p>
+                                Waiting for
+                                <a
+                                    rel="noreferrer"
+                                    target="_blank"
+                                    href={tzlink}
+                                >
+                                    {" "}
+                                    {shortenString(transactionHash, 8)}{" "}
+                                </a>
+                                to be confirmed.
+                            </p>
+                            <p>
+                                You can close this window. The transaction will
+                                be processed in the background.
+                            </p>
                         </Fragment>
-                    }
-                    { step >= 3 &&
-                        <p>Operation successful.</p>
-                    }
-                    { error && 
-                        <p>Failed: {error}</p>}
+                    )}
+                    {step >= 3 && <p>Operation successful.</p>}
+                    {error && <p>Failed: {error}</p>}
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={handleClose} variant="dark">Close</Button>
+                    <Button onClick={handleClose} variant="dark">
+                        Close
+                    </Button>
                 </Modal.Footer>
             </Modal>
-        </Fragment>);
+        </Fragment>
+    );
 }
 
 export default Faucet;
--- src/components/GameButtons.tsx
+++ src/components/GameButtons.tsx
@@ -6,103 +6,107 @@ import { ReactComponent as ClickIcon } from "bootstrap-icons/icons/arrow-right-c
 import { Badge, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
 
 function BetButton(props: any) {
-  return (
-    <Button variant="light" as="span" className="bet-button">
-      <span className="bet-text">BET</span>
-      <ClickIcon className="arrow-icon" />
-    </Button>
-  );
+    return (
+        <Button variant="light" as="span" className="bet-button">
+            <span className="bet-text">BET</span>
+            <ClickIcon className="arrow-icon" />
+        </Button>
+    );
 }
 
 function Odd({ bet, bets }: any) {
-  if (bet === 0) {
-    return (
-      <OverlayTrigger
-        placement="bottom"
-        overlay={
-          <Tooltip>If you bet on this, you'll be the first to do so!</Tooltip>
+    if (bet === 0) {
+        return (
+            <OverlayTrigger
+                placement="bottom"
+                overlay={
+                    <Tooltip>
+                        If you bet on this, you'll be the first to do so!
+                    </Tooltip>
+                }
+            >
+                <p>
+                    <Badge bg="secondary" className="palette-0">
+                        No bet yet
+                    </Badge>
+                </p>
+            </OverlayTrigger>
+        );
+    } else {
+        let colorPalette;
+        const odd = bets / bet;
+        if (odd <= 1) {
+            colorPalette = "palette-0";
         }
-      >
-        <p>
-          <Badge bg="secondary" className="palette-0">
-            No bet yet
-          </Badge>
-        </p>
-      </OverlayTrigger>
-    );
-  } else {
-    let colorPalette;
-    const odd = bets / bet;
-    if (odd <= 1) {
-      colorPalette = "palette-0";
-    }
-    // if(odd <2){
-    //     colorPalette="palette-1";
-    // }
-    // else if(odd <2.5){
-    //     colorPalette="palette-2";
-    // }
-    // else if(odd <3){
-    //     colorPalette="palette-3";
-    // }
-    // else if(odd <5){
-    //     colorPalette="palette-4";
-    // }
-    // else if(odd <8){
-    //     colorPalette="palette-5";
-    // }
-    // else if(odd <10){
-    //     colorPalette="palette-6";
-    // }
-    // else if(odd <50){
-    //     colorPalette="palette-7";
-    // }
-    else {
-      colorPalette = "palette-main";
-      // colorPalette="palette-8";
-    }
-    return (
-      <OverlayTrigger
-        placement="bottom"
-        overlay={
-          <Tooltip>Potential gain for this pool: {bets - bet} XTZ</Tooltip>
+        // if(odd <2){
+        //     colorPalette="palette-1";
+        // }
+        // else if(odd <2.5){
+        //     colorPalette="palette-2";
+        // }
+        // else if(odd <3){
+        //     colorPalette="palette-3";
+        // }
+        // else if(odd <5){
+        //     colorPalette="palette-4";
+        // }
+        // else if(odd <8){
+        //     colorPalette="palette-5";
+        // }
+        // else if(odd <10){
+        //     colorPalette="palette-6";
+        // }
+        // else if(odd <50){
+        //     colorPalette="palette-7";
+        // }
+        else {
+            colorPalette = "palette-main";
+            // colorPalette="palette-8";
         }
-      >
-        <p>
-          <Badge bg="secondary" className={colorPalette}>
-            x{odd.toPrecision(2)}
-          </Badge>
-        </p>
-      </OverlayTrigger>
-    );
-  }
+        return (
+            <OverlayTrigger
+                placement="bottom"
+                overlay={
+                    <Tooltip>
+                        Potential gain for this pool: {bets - bet} XTZ
+                    </Tooltip>
+                }
+            >
+                <p>
+                    <Badge bg="secondary" className={colorPalette}>
+                        x{odd.toPrecision(2)}
+                    </Badge>
+                </p>
+            </OverlayTrigger>
+        );
+    }
 }
 
 function CornerButton({ contractId }: any) {
-  return (
-    <TZKTLink title="View Contract on tzkt.io" id={contractId}>
-      <div className="corner-button">
-        <ContractIcon className="icon" />
-      </div>
-    </TZKTLink>
-  );
+    return (
+        <TZKTLink title="View Contract on tzkt.io" id={contractId}>
+            <div className="corner-button">
+                <ContractIcon className="icon" />
+            </div>
+        </TZKTLink>
+    );
 }
 
 interface CountDownProps {
-  targetDate: Date;
+    targetDate: Date;
 }
 
 function DateSpan(props: CountDownProps) {
-  return (
-    <p className="countDown">
-      {props.targetDate.getHours()}
-      {"h"}
-      {props.targetDate.getMinutes()}{" "}
-      <span style={{ fontWeight: "bold" }}>
-        {props.targetDate.toLocaleDateString()}
-      </span>
-    </p>
-  );
+    return (
+        <p className="countDown">
+            {props.targetDate.getHours()}
+            {"h"}
+            {props.targetDate.getMinutes()}{" "}
+            <span style={{ fontWeight: "bold" }}>
+                {props.targetDate.toLocaleDateString()}
+            </span>
+        </p>
+    );
 }
 
 export { BetButton, Odd, CornerButton, DateSpan };
