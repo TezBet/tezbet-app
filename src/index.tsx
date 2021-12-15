0ea6b490b0479779d3c0a628b7e7c080862c7627
Paralogoss <30406089+Paralogoss@users.noreply.github.com>
2021-12-16 00:10:07 +0100

    Quick fix for CI

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import GameList from "./components/GameList/GameList";
-import Dashboard from "./routes/Dashboard";
 import NotFound from "./routes/NotFound";
 
 ReactDOM.render(
