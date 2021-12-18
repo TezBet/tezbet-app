8a6b020316b4d943ab968726d398fb5c9d9e3e0c
Paralogoss <30406089+Paralogoss@users.noreply.github.com>
2021-12-19 00:00:14 +0100

    Curious fix

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,11 +1,11 @@
-import BigNumber from 'bignumber.js';
-import { Fragment, useCallback, useContext, useEffect, useState } from 'react';
+import BigNumber from "bignumber.js";
+import { Fragment, useCallback, useContext, useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { PlayingCard, ResultCard } from "../components/Dashboard/DashboardItems";
-import { BetModal } from '../components/GameList/BetModal';
+import { BetModal } from "../components/GameList/BetModal";
 import { Game, GamesLoaderReturnType } from "../utils/Game";
-import { compareGames } from '../utils/utils';
-import { WalletContext } from '../utils/WalletContextProvider';
+import { compareGames } from "../utils/utils";
+import { WalletContext } from "../utils/WalletContextProvider";
 import "./Dashboard.css";
 
 function Dashboard({ gamesLoader }: { gamesLoader: GamesLoaderReturnType }) {
@@ -51,41 +51,48 @@ function Dashboard({ gamesLoader }: { gamesLoader: GamesLoaderReturnType }) {
                 });
 
                 setArchive(g);
-            }).catch(console.log);
+            })
+            .catch(console.log);
     }, [Tezos, account, connected]);
 
     useEffect(refreshArchive, [refreshArchive]);
 
     const onBetClick = useCallback((game: Game) => setCurrentGame(game), [setCurrentGame]);
     const onBetClose = useCallback(() => setCurrentGame(undefined), [setCurrentGame]);
-    const onUnBetClick = useCallback((game: Game) => {
-        Tezos.wallet
-            .at(process.env.REACT_APP_TEZBET_CONTRACT!)
-            .then((contract) => contract.methods.unbet_all(game.id).send())
-            .then((op) => op.confirmation())
-            .then((result) => {
-                if (result.completed) {
-                    refreshBalance();
-                } else {
-                    alert("Failed");
-                }
-            });
-    }, [Tezos, refreshBalance]);
+    const onUnBetClick = useCallback(
+        (game: Game) => {
+            Tezos.wallet
+                .at(process.env.REACT_APP_TEZBET_CONTRACT!)
+                .then((contract) => contract.methods.unbet_all(game.id).send())
+                .then((op) => op.confirmation())
+                .then((result) => {
+                    if (result.completed) {
+                        refreshBalance();
+                    } else {
+                        alert("Failed");
+                    }
+                });
+        },
+        [Tezos, refreshBalance]
+    );
 
-    const onRedeem = useCallback((game: Game) => {
-        Tezos.wallet
-            .at(process.env.REACT_APP_TEZBET_CONTRACT!)
-            .then((contract) => contract.methods.redeem_tez(game.id).send())
-            .then((op) => op.confirmation())
-            .then((result) => {
-                if (result.completed) {
-                    refreshBalance();
-                    gamesLoader.refreshGames();
-                } else {
-                    alert("Failed");
-                }
-            });
-    }, [Tezos, refreshBalance, gamesLoader]);
+    const onRedeem = useCallback(
+        (game: Game) => {
+            Tezos.wallet
+                .at(process.env.REACT_APP_TEZBET_CONTRACT!)
+                .then((contract) => contract.methods.redeem_tez(game.id).send())
+                .then((op) => op.confirmation())
+                .then((result) => {
+                    if (result.completed) {
+                        refreshBalance();
+                        gamesLoader.refreshGames();
+                    } else {
+                        alert("Failed");
+                    }
+                });
+        },
+        [Tezos, refreshBalance, gamesLoader]
+    );
 
     if (!connected) {
         return <p>Please connect with your wallet</p>;
@@ -93,13 +100,11 @@ function Dashboard({ gamesLoader }: { gamesLoader: GamesLoaderReturnType }) {
 
     const currentFiltered = gamesLoader.games.filter((game) => game.userBet && game.outcome === -1);
     const redeemableFiltered = gamesLoader.games.filter((game) => game.userBet && game.outcome !== -1);
-    const archiveFiltered = typeof archive != 'undefined' ? archive.filter((game) => game.userBet) : Array<Game>();
+    const archiveFiltered = typeof archive != "undefined" ? archive.filter((game) => game.userBet) : Array<Game>();
 
     return (
         <Fragment>
-            {typeof currentGame != "undefined" && (
-                <BetModal show={true} currentGame={currentGame} onBetClose={onBetClose} />
-            )}
+            {typeof currentGame != "undefined" && <BetModal show={true} currentGame={currentGame} onBetClose={onBetClose} />}
             <Container className="dashboard">
                 <Row>
                     <Col>
@@ -107,12 +112,13 @@ function Dashboard({ gamesLoader }: { gamesLoader: GamesLoaderReturnType }) {
                             <p className="dashboard-title-bold">Current Bets</p>
                         </Row>
                         <Row xs={1} className="g-4">
-                            {currentFiltered.length <=0 && <Col className="dashboard-placeholder">No bet currently open</Col>}
+                            {currentFiltered.length <= 0 && <Col className="dashboard-placeholder">No bet currently open</Col>}
 
-                            {currentFiltered.sort((a, b) => compareGames(a, b))
-                                .map((game) => <PlayingCard game={game}
-                                    onBetClick={onBetClick}
-                                    onUnBetClick={onUnBetClick} />)}
+                            {currentFiltered
+                                .sort((a, b) => compareGames(a, b))
+                                .map((game) => (
+                                    <PlayingCard game={game} onBetClick={onBetClick} onUnBetClick={onUnBetClick} />
+                                ))}
                         </Row>
                     </Col>
                     <Col>
@@ -122,8 +128,11 @@ function Dashboard({ gamesLoader }: { gamesLoader: GamesLoaderReturnType }) {
                         <Row xs={1} className="g-4">
                             {redeemableFiltered.length <= 0 && <Col className="dashboard-placeholder">No bet to redeem</Col>}
 
-                            {redeemableFiltered.sort((a, b) => compareGames(a, b))
-                                .map((game) => <ResultCard game={game} onRedeem={onRedeem} />)}
+                            {redeemableFiltered
+                                .sort((a, b) => compareGames(a, b))
+                                .map((game) => (
+                                    <ResultCard game={game} onRedeem={onRedeem} />
+                                ))}
                         </Row>
                     </Col>
                 </Row>
@@ -131,11 +140,14 @@ function Dashboard({ gamesLoader }: { gamesLoader: GamesLoaderReturnType }) {
                     <p className="dashboard-title-bold">Bets History</p>
                 </Row>
                 <Row xs={1} md={2} className="g-4">
-                    {archiveFiltered.length <= 0 && <Col className="dashboard-placeholder">No bet in history</Col>}
+                    {archiveFiltered.length <= 0 && <Col className="dashboard-placeholder">No bet history</Col>}
 
-                    {archiveFiltered.sort((a, b) => compareGames(a, b))
-                        .map((game) => <ResultCard game={game} onRedeem={onRedeem} />)}
-                        </Row>
+                    {archiveFiltered
+                        .sort((a, b) => compareGames(a, b))
+                        .map((game) => (
+                            <ResultCard game={game} onRedeem={onRedeem} />
+                        ))}
+                </Row>
             </Container>
         </Fragment>
     );
